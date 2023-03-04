d6eb0672cca1bf589d6b49f0d5ba8a52fd04c51a
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-03-04 10:42:43 +0100

    Day 16

--- 16_Higher_Order_Component/example1/src/App.js
+++ 16_Higher_Order_Component/example1/src/App.js
@@ -1,28 +1,95 @@
 // src/App.js
 // Recommended for most of the cases
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+const Button = ({ onClick, text, style }) => {
+    return (
+        <button onClick={onClick} style={style}>
+            {text}
+        </button>
+    )
+}
+
+const buttonWithSuperPower = (CompParam, name = 'default') => {
+    const colors = [
+        {
+            name: 'default',
+            backgroundColor: '#e7e7e7',
+            color: '#000000',
+        },
+        {
+            name: 'react',
+            backgroundColor: '#61dbfb',
+            color: '#ffffff',
+        },
+        {
+            name: 'success',
+            backgroundColor: '#4CAF50',
+            color: '#ffffff',
+        },
+        {
+            name: 'info',
+            backgroundColor: '#2196F3',
+            color: '#ffffff',
+        },
+        {
+            name: 'warning',
+            backgroundColor: '#ff9800',
+            color: '#ffffff',
+        },
+        {
+            name: 'danger',
+            backgroundColor: '#f44336',
+            color: '#ffffff',
+        },
+    ]
+    const { backgroundColor, color } = colors.find((c) => c.name === name)
+
+    const buttonStyles = {
+        backgroundColor,
+        padding: '10px 45px',
+        border: 'none',
+        borderRadius: 3,
+        margin: 3,
+        cursor: 'pointer',
+        fontSize: '1.25rem',
+        color,
+    }
+    return (props) => {
+        return <CompParam {...props} style={buttonStyles} />
+    }
+}
+
+const NewButton = buttonWithSuperPower(Button)
+const ReactButton = buttonWithSuperPower(Button, 'react')
+const InfoButton = buttonWithSuperPower(Button, 'info')
+const SuccessButton = buttonWithSuperPower(Button, 'success')
+const WarningButton = buttonWithSuperPower(Button, 'warning')
+const DangerButton = buttonWithSuperPower(Button, 'danger')
 
 class App extends Component {
-    state = {
-        pos : {
-            x : 0,
-            y : 0
-        }
-    }
-  
-    move = (e) => {
-        this.setState({
-            pos : {
-                x : Math.floor(Math.random()*window.innerWidth),
-                y : Math.floor(Math.random()*window.innerHeight)
-            }
-        })
-    }
-
     render() {
         return (
-            <div>
-                <h1 style= {{"top" : this.state.pos.y+"px","left" : this.state.pos.x+"px","position":'absolute'}} onMouseEnter={this.move}>App Component</h1>
+            <div className='App'>
+                <Button text='No Style' onClick={() => alert('I am not styled yet')} />
+                <NewButton
+                    text='Styled Button'
+                    onClick={() => alert('I am the default style')}
+                />
+                <ReactButton text='React' onClick={() => alert('I have react color')} />
+                <InfoButton
+                    text='Info'
+                    onClick={() => alert('I am styled with info color')}
+                />
+                <SuccessButton text='Success' onClick={() => alert('I am successful')} />
+                <WarningButton
+                    text='Warning'
+                    onClick={() => alert('I warn you many times')}
+                />
+                <DangerButton
+                    text='Danger'
+                    onClick={() => alert('Oh no, you can not restore it')}
+                />
             </div>
         )
     }
