9d11d4f812dbb464e9541cab810e47580a8a1044
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-03-04 11:12:19 +0100

    Day 17

--- 17_React_Router/example1/src/App.js
+++ 17_React_Router/example1/src/App.js
@@ -1,29 +1,57 @@
 // src/App.js
 // Recommended for most of the cases
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    NavLink,
+} from 'react-router-dom'
+
+// Home component
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+    <div>
+        <h1>30 Days Of React Challenge</h1>
+    </div>
+)
+
+const NotFound = (props) => <h1>The page your looking for not found</h1>
 
 class App extends Component {
-    state = {
-        pos : {
-            x : 0,
-            y : 0
-        }
-    }
-  
-    move = (e) => {
-        this.setState({
-            pos : {
-                x : Math.floor(Math.random()*window.innerWidth),
-                y : Math.floor(Math.random()*window.innerHeight)
-            }
-        })
-    }
-
     render() {
         return (
-            <div>
-                <h1 style= {{"top" : this.state.pos.y+"px","left" : this.state.pos.x+"px","position":'absolute'}} onMouseEnter={this.move}>App Component</h1>
-            </div>
+            <BrowserRouter>
+                <div className='App'>
+                    <ul>
+                        <li>
+                            <NavLink to='/'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about'>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contact'>Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/challenges'>Challenges</NavLink>
+                        </li>
+                    </ul>
+
+                    <Routes>
+                        <Route path='/about' element={<About />} />
+                        <Route path='/contact' element={<Contact />} />
+                        <Route path='/challenges' element={<Challenges />} />
+                        <Route path='/' element={<Home />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         )
     }
 }
